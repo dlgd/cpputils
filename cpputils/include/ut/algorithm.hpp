d70d825ac06b4b6dfc4df45187f549e335b49b6c
Daniel Gazard <daniel.gazard@gmail.com>
2015-03-01 22:39:56 +0100

    Add fold_equal

--- cpputils/include/ut/algorithm.hpp
+++ cpputils/include/ut/algorithm.hpp
@@ -18,6 +18,53 @@ struct less_t
    }
 };
 
+template <typename BinaryOp>
+struct zip_op_t
+{
+   zip_op_t(BinaryOp op):
+      m_op(op)
+   {}
+
+   template <typename T1, typename T2, typename OutputIteratorType>
+   OutputIteratorType operator()(OutputIteratorType output,
+                                 T1 lhs, T2 rhs) const
+   {
+      *output = m_op(lhs, rhs);
+      return ++output;
+   }
+
+   BinaryOp m_op;
+};
+
+}
+
+template <typename IteratorType1, typename IteratorType2,
+          typename T, typename BinaryOp, typename Less>
+T fold_equal(IteratorType1 begin1, IteratorType1 end1,
+             IteratorType2 begin2, IteratorType2 end2,
+             T init, BinaryOp op, Less less)
+{
+   IteratorType1 it1 = begin1;
+   IteratorType2 it2 = begin2;
+   while (it1 != end1 || it2 != end2)
+   {
+      if (it2 == end2 || less(*it1, *it2))
+         init = op(init, &*(it1++), nullptr);
+      else if (it1 == end1 || less(*it2, *it1))
+         init = op(init, nullptr, &*(it2++));
+      else
+         init = op(init, &*(it1++), &*(it2++));
+   }
+   return init;
+}
+
+template <typename IteratorType1, typename IteratorType2,
+          typename T, typename BinaryOp>
+T fold_equal(IteratorType1 begin1, IteratorType1 end1,
+             IteratorType2 begin2, IteratorType2 end2,
+             T init, BinaryOp op)
+{
+   fold_equal(begin1, end1, begin2, end2, init, op, detail::less_t());
 }
 
 template <typename IteratorType1, typename IteratorType2,
@@ -26,18 +73,8 @@ void zip_equal(IteratorType1 begin1, IteratorType1 end1,
                IteratorType2 begin2, IteratorType2 end2,
                OutputIteratorType output, BinaryOp op, Less less)
 {
-   IteratorType1 it1 = begin1;
-   IteratorType2 it2 = begin2;
-
-   while (it1 != end1 || it2 != end2)
-   {
-      if (it2 == end2 || less(*it1, *it2))
-         output = op(&*(it1++), nullptr);
-      else if (it1 == end1 || less(*it2, *it1))
-         output = op(nullptr, &*(it2++));
-      else
-         output = op(&*(it1++), &*(it2++));
-   }
+   detail::zip_op_t<BinaryOp> zip_op(op);
+   fold_equal(begin1, end1, begin2, end2, output, zip_op, less);
 }
 
 template <typename IteratorType1, typename IteratorType2,
